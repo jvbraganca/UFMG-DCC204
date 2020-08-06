e331125ba67efb5bb23d3922187b8e66bb3cede9
Joao Braganca <jvbragancaa@gmail.com>
2020-08-06 13:30:45 -0300

    VPLs

---
+++ CPP/Modulo 1/VPL 1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+    std::string helloWorld = "Hello World";
+    std::cout << helloWorld << std::endl;
+    return 0;
+}
---
+++ CPP/Modulo 1/VPL 2/main.cpp
@@ -0,0 +1,33 @@
+#include <cle>
+#include <string>
+
+void printer(int size)
+{
+    char star = '*'; 
+    std::cout << std::string(size, star) << std::endl;
+}
+
+int main()
+{
+    int height;
+    int printValue = 1;
+    bool hasReachedMaxHeight = false;
+
+    std::cout << "Insira a altura maxima da piramide: ";
+    std::cin >> height;
+
+
+    for (int i = 0; i < ((height*2)-1); i++)
+    {
+        if (printValue < height && !hasReachedMaxHeight)
+        {
+            printer(printValue);
+            printValue++;
+            hasReachedMaxHeight = (printValue == height) ? true : false;
+        } else
+        {
+            printer(printValue);
+            printValue--;
+        }
+    }
+}
