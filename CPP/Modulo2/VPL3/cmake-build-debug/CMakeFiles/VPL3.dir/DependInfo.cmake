ecd7c730a79c58bb3da00abad5e78258b4c3da8a
Joao Bragança <jvbragancaa@gmail.com>
2020-09-20 15:43:23 -0300

    Update

---
+++ CPP/Modulo2/VPL3/cmake-build-debug/CMakeFiles/VPL3.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/UFMG/UFMG-DCC204/CPP/Modulo2/VPL3/Vec1D.cpp" "D:/UFMG/UFMG-DCC204/CPP/Modulo2/VPL3/cmake-build-debug/CMakeFiles/VPL3.dir/Vec1D.cpp.obj"
+  "D:/UFMG/UFMG-DCC204/CPP/Modulo2/VPL3/Vec2D.cpp" "D:/UFMG/UFMG-DCC204/CPP/Modulo2/VPL3/cmake-build-debug/CMakeFiles/VPL3.dir/Vec2D.cpp.obj"
+  "D:/UFMG/UFMG-DCC204/CPP/Modulo2/VPL3/Vec3D.cpp" "D:/UFMG/UFMG-DCC204/CPP/Modulo2/VPL3/cmake-build-debug/CMakeFiles/VPL3.dir/Vec3D.cpp.obj"
+  "D:/UFMG/UFMG-DCC204/CPP/Modulo2/VPL3/main.cpp" "D:/UFMG/UFMG-DCC204/CPP/Modulo2/VPL3/cmake-build-debug/CMakeFiles/VPL3.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CPP/Modulo2/VPL3/cmake-build-debug/CMakeFiles/VPL3.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VPL3.dir/Vec1D.cpp.obj"
+  "CMakeFiles/VPL3.dir/Vec2D.cpp.obj"
+  "CMakeFiles/VPL3.dir/Vec3D.cpp.obj"
+  "CMakeFiles/VPL3.dir/main.cpp.obj"
+  "VPL3.exe"
+  "VPL3.exe.manifest"
+  "VPL3.pdb"
+  "libVPL3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VPL3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
