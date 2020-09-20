712e66ab6340d4e072cff9d4308f80304e5bace5
Joao Bragança <jvbragancaa@gmail.com>
2020-09-20 15:44:07 -0300

    Added VPL4

---
+++ CPP/Modulo2/VPL4/CommaDecorator.cpp
@@ -0,0 +1,12 @@
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <regex>
+#include "CommaDecorator.hpp"
+
+std::string CommaDecorator::toString() {
+    if (/*[a-z]{1,}|[][0-9\.]{1,}*/)
+    // Implemente
+    return "";
+}
+
